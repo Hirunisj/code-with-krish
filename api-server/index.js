70ac6316d598c91c803c166e8058754365e85f58
Hiruni <hirunisudeepaj@gmail.com>
2025-02-26 22:18:33 +0530

    avg sort search done

--- api-server/index.js
+++ api-server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {minNumber, maxNumber} = require('./util');
+const {minNumber, maxNumber, avgValue, sortArray, searchNumber} = require('./util');
 
 const app = new express();
 const port = 3000;
@@ -12,6 +12,18 @@ app.get('/number/min', (req, res) => {
 app.get('/number/max', (req, res) => {
    const maxNum = maxNumber(req, res);
 })
+
+app.get('/number/avg' , (req,res) =>{
+    const avg = avgValue (req,res);
+})
+
+app.get('/number/sort', (req, res) => {
+    const sortedArray = sortArray(req, res);
+});
+
+// app.get('/number/search', (req, res) => {
+//     const searchResult = searchNumber(req, res);
+// });
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
--- api-server/util.js
+++ api-server/util.js
@@ -14,8 +14,7 @@ function minNumber(req, res){
     })
 
 }
-const maxNumber = (req, res) => {
-
+function maxNumber (req, res) {
     const num1 = parseFloat(req.query.num1);
     const num2 = parseFloat(req.query.num2);
 
@@ -24,12 +23,73 @@ const maxNumber = (req, res) => {
             error : "Invalid Input"
         })
     }
-
-
-    let maxNo = Math.max(num1, num2);
+    const maxNo = Math.max(num1, num2);
     return res.status(200).send({
         data : maxNo
     })
 
 }
-module.exports = {minNumber, maxNumber};
+
+function avgValue(req, res) {
+    const nums = req.query.num;
+    const numArray = nums.split(',').map(num => parseFloat(num));
+ 
+     if (numArray.some(num => isNaN(num))) {
+          return res.status(400).send({
+               error: "Invalid Input"
+          });
+     }
+ 
+     const avg = numArray.reduce((sum, num) => sum + num, 0) / numArray.length;
+     return res.status(200).send({
+         data: avg
+     });
+ }
+
+ function sortArray(req, res) {
+    const nums = req.query.numbers;
+    const order = req.query.order;
+
+    if (!nums) {
+        return res.status(400).send({
+            error: "Invalid Input"
+        });
+    }
+
+    const numArray = nums.split(',').map(num => parseFloat(num));
+
+    if (numArray.some(num => isNaN(num))) {
+        return res.status(400).send({
+            error: "Invalid Input"
+        });
+    }
+
+    if (order !== 'asc' && order !== 'desc') {
+        return res.status(400).send({
+            error: "Invalid Order"
+        });
+    }
+
+    numArray.sort((a, b) => order === 'asc' ? a - b : b - a);
+
+    return res.status(200).send({
+        data: numArray
+    });
+}
+
+// function searchNumber(req, res) {
+//     const text = req.query.text;
+//     const target = req.query.target;
+
+//     if (!text || !target) {
+//         return res.status(400).send({
+//             error: "Text and target are required"
+//         });
+//     }
+    
+     
+// }
+
+
+
+module.exports = {minNumber, maxNumber, avgValue, sortArray, searchNumber};
